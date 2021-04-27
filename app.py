db06b599f834c4c69dcbf551ad5f527030469337
Chelsea Chaussee <cfchaussee@gmail.com>
2021-04-26 21:20:50 -0700

    fixed app flask route errors

--- app.py
+++ app.py
@@ -14,10 +14,12 @@ marathondata = [runner for runner in marathon_collection.find()]
    
 @app.route('/', methods=['GET'])
 def home():
-    boston = list(marathondata)
-    return current_app.response_class(dumps(boston), mimetype="application/json")
+    return render_template("index.html")
 
-    
+@app.route('/api', methods=['GET'])
+def home():
+    boston = list(marathondata)
+    return current_app.response_class(dumps(boston), mimetype="application/json")    
 
 if __name__ == "__main__":
     app.run(debug=True)
